58a842dc611fa79fb12f79e8a3f08b395f1bdb55
Zak Fenton <zak.fenton@outlook.com>
2021-01-25 00:03:35 +1100

    Made test script slightly more user-friendly

--- SimpleCore-tests.sh
+++ SimpleCore-tests.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
+echo "### Compiling Tests... ###"
 iverilog -o SimpleCore-tests SimpleTests.v SimpleMCU.v SimpleCore.v
 echo "### Sleeping a few seconds in case you wanna CTRL+C ###"
 sleep 5
+echo "### Starting tests... ###"
 vvp SimpleCore-tests
